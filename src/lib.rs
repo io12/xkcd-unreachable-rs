4b462fdbcbfb998efbbc5f123ebf553ec075cd6f
Benjamin Levy <blevy@protonmail.com>
2020-03-16 01:00:24 -0400

    Add docs

--- src/lib.rs
+++ src/lib.rs
@@ -21,10 +21,54 @@ mod tests {
     }
 }
 
-macro_rules! def_unreachable {
-    ($dollar:tt, $name:ident, $main_msg:expr) => {
+/// Helper macro to define `xkcd_unreachable!()`. This avoids having to
+/// copy-paste the error text for each variant.
+///
+/// ## Parameters:
+///   * `$dollar:tt`
+///     * A dollar sign token (`$`). This exists to allow higher-order
+///       macro definition.
+///       <https://twitter.com/comex/status/979949654610604032>
+///   * `$main_msg:expr`
+///     * The text of the xkcd comic ("If you're seeing this, ...")
+macro_rules! def_xkcd_unreachable {
+    ($dollar:tt, $main_msg:expr) => {
         #[macro_export]
-        macro_rules! $name {
+        /// Macro indicating unreachable code. This is equivalent to
+        /// [`unreachable!()`](core::unreachable!), except that it displays the
+        /// message in (xkcd 2200)[https://xkcd.com/2200/].
+        ///
+        /// ## Example
+        /// ```
+        /// fn foo(x: Option<i32>) {
+        ///     match x {
+        ///         Some(n) if n >= 0 => println!("Some(Non-negative)"),
+        ///         Some(n) if n <  0 => println!("Some(Negative)"),
+        ///         Some(_)           => xkcd_unreachable!(),
+        ///         None              => println!("None")
+        ///     }
+        /// }
+        /// ```
+        ///
+        /// ## Output if reached
+        /// ```
+        /// ⚠ ERROR
+        ///
+        /// If you're seeing this, the code is in what
+        /// I thought was an unreachable state.
+        ///
+        /// I could give you advice for what to do.
+        /// But honestly, why should you trust me?
+        /// I clearly screwed this up. I'm writing a
+        /// message that should never appear, yet
+        /// I know it will probably appear someday.
+        ///
+        /// On a deep level, I know I'm not
+        /// up to this task. I'm so sorry.
+        ///
+        /// <https://xkcd.com/2200/>
+        /// ```
+        macro_rules! xkcd_unreachable {
             () => {
                 panic!($main_msg)
             };
@@ -38,9 +82,8 @@ macro_rules! def_unreachable {
     };
 }
 
-def_unreachable!(
+def_xkcd_unreachable!(
     $,
-    xkcd_unreachable,
     concat!(
         "\n",
         "⚠ ERROR\n",
